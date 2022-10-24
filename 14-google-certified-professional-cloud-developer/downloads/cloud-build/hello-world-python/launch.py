9f380723b23473ca1014e19ca9395ec0fd486884
Ranga Karanam <ranga@Rangas-MacBook-Pro-2.local>
2022-10-24 08:21:31 +0530

    Google Cloud Updates - 2022 October

--- 14-google-certified-professional-cloud-developer/downloads/cloud-build/hello-world-python/launch.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-helloworld = Flask(__name__)
-@helloworld.route("/")
-def run():
-    return "{\"message\":\"Hello World Python v1\"}"
-if __name__ == "__main__":
-    helloworld.run(host="0.0.0.0", port=int("8080"), debug=True)
